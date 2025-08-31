6e0aa4b3b7f6e2dd3db4925195318ea177487a9e
Zach Breeden <zachbreeden1976@icloud.com>
2025-08-31 18:01:20 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 
 export default defineConfig({
-  base: '/bank-model/',           // project page base
+  base: '/bank-model/',       // GitHub Pages project base
   server: { open: true },
   build: {
     outDir: 'docs',
@@ -12,7 +12,7 @@ export default defineConfig({
         main: resolve(__dirname, 'index.html'),
         throughput: resolve(__dirname, 'throughput.html'),
         risk: resolve(__dirname, 'risk.html'),
-        value: resolve(__dirname, 'value.html')
+        value: resolve(__dirname, 'value.html'),
       }
     }
   }
